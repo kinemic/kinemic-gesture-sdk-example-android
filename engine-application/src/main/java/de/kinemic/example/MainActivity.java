568b89c02978368607c109aaa1d40e53510556e6
Marcus Georgi <marcus.georgi@kinemic.de>
2019-03-07 16:12:30 +0100

    Introduce new Features from SDK 0.10.2
      - Uses Gesture Common Support Library for UI Elements
      - Uses Gesture Common for EngineProvider and Searching
      - Introduction of AirMouse for PDF Viewer Manipulation
      - Enums make handling of events more comfortable

--- engine-application/src/main/java/de/kinemic/example/MainActivity.java
+++ engine-application/src/main/java/de/kinemic/example/MainActivity.java
@@ -1,81 +1,108 @@
 package de.kinemic.example;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import de.kinemic.gesture.ActivationState;
-import de.kinemic.gesture.Engine;
+import de.kinemic.gesture.ConnectionReason;
 import de.kinemic.gesture.ConnectionState;
-import de.kinemic.gesture.EnumNames;
+import de.kinemic.gesture.Engine;
+import de.kinemic.gesture.Gesture;
 import de.kinemic.gesture.OnActivationStateChangeListener;
 import de.kinemic.gesture.OnBatteryChangeListener;
 import de.kinemic.gesture.OnConnectionStateChangeListener;
 import de.kinemic.gesture.OnGestureListener;
+import de.kinemic.gesture.OnStreamQualityChangeListener;
+import de.kinemic.gesture.common.EngineProvider;
+import de.kinemic.gesture.common.fragments.BandFloatingActionButtonFragment;
+import de.kinemic.gesture.common.fragments.BandMenuFragment;
+import de.kinemic.gesture.common.fragments.GestureFloatingActionButtonFragment;
 
-import javax.annotation.Nullable;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnActivationStateChangeListener,
+        OnBatteryChangeListener, OnConnectionStateChangeListener, OnGestureListener, OnStreamQualityChangeListener {
 
   private static final String TAG = MainActivity.class.getSimpleName();
 
-  /** Engine object we obtain from out Activity */
-  private @Nullable Engine mEngine;
+  private Engine mEngine;
 
-  /** State which indicates which action to perfom on FAB press */
-  private boolean mFabDisconnects = false;
+  private PDFViewAirmouseAdapter mAirmouseAdapter;
 
-  /** FAB to search sensors or disconnect current sensor */
-  @BindView(R.id.fab)
-  FloatingActionButton mFabButton;
-
-  @BindView(R.id.child_button)
-  Button mChildButton;
+  @BindView(R.id.fabSensor) FloatingActionButton mFabButton;
+  @BindView(R.id.fabGestureDark) FloatingActionButton mFabGestureDark;
+  @BindView(R.id.pdfView) PDFView mPdfView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
     ButterKnife.bind(this);
 
-    // This permission is required to find bluetooth devices
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
       }
     }
 
-    // Get the engine instance from our application
-    mEngine = EngineApplication.getEngine(this, true);
+    mEngine = ((EngineProvider) getApplication()).getEngine();
 
-    Intent intent = getIntent();
-    if (intent != null && intent.hasExtra("TITLE")) {
-      setTitle(intent.getStringExtra("TITLE"));
-      mChildButton.setVisibility(View.GONE);
+    if (savedInstanceState == null) {
+      // this fragment manages a sensor menu icon which depicts the sensor state with its icon and shows a info dialog on click
+      getSupportFragmentManager().beginTransaction().add(BandMenuFragment.newInstance(BandMenuFragment.ConnectMode.CHOOSE), "sensor_menu").commit();
     }
+
+    if (savedInstanceState == null) {
+      // this fragment manages a sensor fab icon which depicts the sensor state with its icon and shows a info dialog on click
+      BandFloatingActionButtonFragment bandFabFragment = BandFloatingActionButtonFragment.newInstance(BandFloatingActionButtonFragment.ConnectMode.CHOOSE);
+      getSupportFragmentManager().beginTransaction().add(bandFabFragment, "band_fab").commit();
+      bandFabFragment.setFloatingActionButton(mFabButton);
+
+      // this fragment manages a gesture fab and depicts gestures with it's assigned floating action button
+      GestureFloatingActionButtonFragment gestureFabFragmentDark = GestureFloatingActionButtonFragment.newInstance(true);
+      getSupportFragmentManager().beginTransaction().add(gestureFabFragmentDark, "gesture_fab_dark").commit();
+      gestureFabFragmentDark.setFloatingActionButton(mFabGestureDark);
+    } else {
+      BandFloatingActionButtonFragment bandFabFragment = (BandFloatingActionButtonFragment) getSupportFragmentManager().findFragmentByTag("band_fab");
+      if (bandFabFragment != null) bandFabFragment.setFloatingActionButton(mFabButton);
+
+      GestureFloatingActionButtonFragment gestureFabFragmentDark = (GestureFloatingActionButtonFragment) getSupportFragmentManager().findFragmentByTag("gesture_fab_dark");
+      if (gestureFabFragmentDark != null) gestureFabFragmentDark.setFloatingActionButton(mFabGestureDark);
+    }
+
+    mAirmouseAdapter = new PDFViewAirmouseAdapter(this, mEngine, mPdfView);
+
+    mPdfView.fromAsset("Sample.pdf")
+            .pages(0)
+            .onDraw(mAirmouseAdapter) /* debug */
+            .load();
   }
 
   @Override
   protected void onPause() {
-    // unregister handlers on pause
-    mEngine.unregisterOnConnectionStateChangeListener(mConnectionStateListener);
-    mEngine.unregisterOnGestureListener(mGestureListener);
-    mEngine.unregisterOnActivationStateChangeListener(mActivationStateChangeListener);
-    mEngine.unregisterOnBatteryChangeListener(mBatteryChangeListener);
+    mEngine.unregisterOnBatteryChangeListener(this);
+    mEngine.unregisterOnStreamQualityChangeListener(this);
+    mEngine.unregisterOnActivationStateChangeListener(this);
+    mEngine.unregisterOnConnectionStateChangeListener(this);
+    mEngine.unregisterOnGestureListener(this);
+    mEngine = null;
 
     super.onPause();
   }
@@ -84,89 +111,54 @@ public class MainActivity extends AppCompatActivity {
   protected void onResume() {
     super.onResume();
 
-    // ensure that we have a engine (might have been released when the user left the app)
-    mEngine = EngineApplication.getEngine(this, true);
-
-    // register handlers on resume
-    mEngine.registerOnConnectionStateChangeListener(mConnectionStateListener);
-    mEngine.registerOnGestureListener(mGestureListener);
-    mEngine.registerOnActivationStateChangeListener(mActivationStateChangeListener);
-    mEngine.registerOnBatteryChangeListener(mBatteryChangeListener);
-
-    updateFabButton(mEngine.getConnectionState());
-  }
-
-  private void updateFabButton(@ConnectionState int connectionState) {
-    // Change the action of the FAB depending of the connection state
-    mFabDisconnects = connectionState != ConnectionState.DISCONNECTED;
-    mFabButton.setImageResource(mFabDisconnects ? R.drawable.ic_close_white_24dp : R.drawable.ic_search_white_24dp);
+    mEngine = ((EngineProvider) getApplication()).getEngine();
+    mEngine.registerOnBatteryChangeListener(this);
+    mEngine.registerOnStreamQualityChangeListener(this);
+    mEngine.registerOnActivationStateChangeListener(this);
+    mEngine.registerOnConnectionStateChangeListener(this);
+    mEngine.registerOnGestureListener(this);
   }
 
   @Override
-  protected void onDestroy() {
-    super.onDestroy();
+  public void onBatteryChanged(int batteryPercent) {
+    Snackbar.make(mFabButton, "Battery: " + batteryPercent, Snackbar.LENGTH_SHORT).show();
+    Log.i(TAG, "battery changed: " + batteryPercent);
   }
 
-  private OnBatteryChangeListener mBatteryChangeListener = new OnBatteryChangeListener() {
-    @Override
-    public void onBatteryChanged(int batteryPercent) {
-      Snackbar.make(mFabButton, "Battery: " + batteryPercent, Snackbar.LENGTH_SHORT).show();
-      Log.i(TAG, "battery changed: " + batteryPercent);
-    }
-  };
+  @Override
+  public void onStreamQualityChanged(int quality) {
+    Snackbar.make(mFabButton, "Stream Quality: " + quality, Snackbar.LENGTH_SHORT).show();
+    Log.i(TAG, "Stream Quality changed: " + quality);
+  }
 
-  private OnActivationStateChangeListener mActivationStateChangeListener = new OnActivationStateChangeListener() {
-    @Override
-    public void onActivationStateChanged(int state) {
-      Snackbar.make(mFabButton, "ActivationState: " + nameOfAS(state), Snackbar.LENGTH_SHORT).show();
-      Log.i(TAG, "activation state changed: " + nameOfAS(state));
-    }
-  };
+  @Override
+  public void onActivationStateChanged(@NonNull ActivationState state) {
+    Log.i(TAG, "activation state changed: " + state);
+  }
+  @Override
+  public void onConnectionStateChanged(@NonNull ConnectionState state, @NonNull ConnectionReason reason) {
+    Log.i(TAG, "connection state changed: " + state.toString() + " (" + reason.toString() + ")");
+  }
 
-  private OnConnectionStateChangeListener mConnectionStateListener = new OnConnectionStateChangeListener() {
-    @Override
-    public void onConnectionStateChanged(int state) {
-      updateFabButton(state);
+  @Override
+  public void onGesture(@NonNull Gesture gesture) {
+    mEngine.buzz(150);
+    Log.i(TAG, "got gesture: " + gesture);
 
-      Snackbar.make(mFabButton, "ConnectionState: " + EnumNames.fromConnectionState(state), Snackbar.LENGTH_SHORT).show();
-      Log.i(TAG, "connection state changed: " + EnumNames.fromConnectionState(state));
-    }
-  };
-
-  private OnGestureListener mGestureListener = new OnGestureListener() {
-    @Override
-    public void onGesture(int gesture) {
-      Snackbar.make(mFabButton, "Gesture: " + EnumNames.fromGesture(gesture), Snackbar.LENGTH_SHORT).show();
-      Log.i(TAG, "got gesture: " + EnumNames.fromGesture(gesture));
-    }
-  };
-
-  /** small helper for readable activation states */
-  private String nameOfAS(@ActivationState int state) {
-    switch (state) {
-      case ActivationState.ACTIVE:
-        return "Active";
-      case ActivationState.INACTIVE:
-        return "Inactive";
-      default:
-        return "Unknown"; // will never happen
+    if (gesture == Gesture.ROTATE_RL) {
+      if (mAirmouseAdapter.isAirmouseActive()) return;
+      mAirmouseAdapter.setAirmouseActive(true);
+      Toast.makeText(this, "AirMouse started", Toast.LENGTH_SHORT).show();
+    } else if (gesture == Gesture.ROTATE_LR) {
+      if (!mAirmouseAdapter.isAirmouseActive()) return;
+      mAirmouseAdapter.setAirmouseActive(false);
+      Toast.makeText(this, "AirMouse stopped", Toast.LENGTH_SHORT).show();
     }
   }
 
   @OnClick(R.id.buzz_button)
   public void buzz() {
-
-    // use vibration as haptic feedback
-    mEngine.buzz(400);
-  }
-
-  @OnClick(R.id.child_button)
-  public void navigateToChild() {
-
-    // change to a new activity to showcase the shared engine
-    Intent intent = new Intent(this, MainActivity.class);
-    intent.putExtra("TITLE", "Child");
-    startActivity(intent);
+    mEngine.buzz(300);
   }
 
   @OnCheckedChanged({R.id.red_box, R.id.green_box, R.id.blue_box})
@@ -175,44 +167,7 @@ public class MainActivity extends AppCompatActivity {
     boolean green = ((CheckBox) findViewById(R.id.green_box)).isChecked();
     boolean blue = ((CheckBox) findViewById(R.id.blue_box)).isChecked();
 
-    // use LED as visual feedback
     mEngine.setLed(red, green, blue);
-  }
-
-  @OnClick(R.id.fab)
-  public void fabButtonClicked(View view) {
-    if (mFabDisconnects) {
-      Snackbar.make(view, "Disconnect...", Snackbar.LENGTH_SHORT).show();
-      mEngine.disconnect();
-    } else {
-      showSearchDialog();
-    }
-  }
-
-
-  /** show a list of sensors and connect to selected one */
-  private void showSearchDialog() {
-    // List adapter to manage search results. Implements SearchCallback.
-    SearchResultAdapter adapter = new SearchResultAdapter(this);
-
-    AlertDialog.Builder builder = new AlertDialog.Builder(this)
-        .setIcon(de.kinemic.gesture.R.drawable.ic_search_black_24dp)
-        .setTitle("Select Sensor")
-        .setPositiveButton("Clear", null)
-        .setNegativeButton("Cancel", null)
-        .setOnDismissListener(dialogInterface -> mEngine.stopSearch())
-        .setAdapter(adapter, (dialog, which) -> {
-          // Sensor was selected in list -> connect to it
-          mEngine.stopSearch();
-          mEngine.connect(adapter.getItem(which).address);
-    });
-
-    AlertDialog dialog = builder.show();
-
-    // reuse the positive button to non closing action (needs to be done here to not close)
-    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(view -> adapter.clear());
-
-    mEngine.startSearch(adapter);
   }
 
 }
---
+++ engine-application/src/main/java/de/kinemic/example/PDFViewAirmouseAdapter.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2018 Kinemic GmbH. All rights reserved.
+ */
+
+package de.kinemic.example;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+
+import de.kinemic.gesture.AirmousePalmDirection;
+import de.kinemic.gesture.Engine;
+import de.kinemic.gesture.OnAirmouseEventListener;
+
+public class PDFViewAirmouseAdapter implements OnAirmouseEventListener, OnDrawListener, LifecycleObserver {
+    private static final String TAG = PDFViewAirmouseAdapter.class.getSimpleName();
+
+    private final static float MAX_ZOOM = 14f;
+    private final static float MIN_ZOOM = 1f;
+    private final static long REFRESH_TIMEOUT = 250L;
+
+    private final static float ZOOM_THRESHOLD = 0.01f;
+
+    /* these may need some adjusting to have a good feel */
+    private final static float PAN_FACTOR = 25f;
+    private final static float ZOOM_FACTOR = 0.025f;
+
+    private Engine engine;
+    private PDFView pdfView;
+    private Handler handler;
+
+    private boolean airmouseValid = false;
+    private float airmouseX, airmouseY;
+    private AirmousePalmDirection airmouseMode = AirmousePalmDirection.INCONCLUSIVE;
+    private boolean airmouseActive = false;
+
+    private final Paint pointerPaint = new Paint();
+    private final Paint bandPaint = new Paint();
+
+    private Runnable refresher = () -> pdfView.loadPages();
+    
+    public PDFViewAirmouseAdapter(LifecycleOwner owner, Engine engine, PDFView pdfView) {
+        pointerPaint.setColor(Color.RED);
+        bandPaint.setColor(Color.BLUE);
+        
+        handler = new Handler();
+        this.engine = engine;
+        this.pdfView = pdfView;
+
+        owner.getLifecycle().addObserver(this);
+    }
+
+    public boolean isAirmouseActive() {
+        return airmouseActive;
+    }
+
+    public void setAirmouseActive(boolean active) {
+        if (active == airmouseActive) return;
+        if (active) {
+            airmouseActive = true;
+            engine.startAirmouse();
+            Log.d(TAG, "airmouse started");
+        } else {
+            airmouseActive = false;
+            engine.stopAirmouse();
+            Log.d(TAG, "airmouse stopped");
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private void registerListeners() {
+        engine.registerOnAirmouseEventListener(this);
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private void unregisterListeners() {
+        engine.unregisterOnAirmouseEventListener(this);
+    }
+
+    /* used to correctly calculate dx when x wraps from -180 to 180 and 180 to -180 */
+    private float wrapDx(float dx) {
+        if (dx > 180.0) return dx - 360.0f;
+        else if (dx < -180.0) return dx + 360.0f;
+        else return dx;
+    }
+
+    private void refreshWhenIdle() {
+        handler.removeCallbacks(refresher);
+        handler.postDelayed(refresher, REFRESH_TIMEOUT);
+    }
+
+    @Override
+    public void onMove(float x, float y, @NonNull AirmousePalmDirection palmFacing) {
+        //Log.d(TAG, "onMove: " + x + ", " + y + ", " + EnumNames.fromFacing(palmFacing));
+        if (!airmouseValid || palmFacing != airmouseMode) {
+            airmouseX = x;
+            airmouseY = y;
+            airmouseValid = true;
+            airmouseMode = palmFacing;
+            pdfView.invalidate(); // for onDraw
+            return;
+        }
+
+        float dx = wrapDx(airmouseX - x);
+        float dy = (airmouseY - y);
+
+        //Log.v(TAG, String.format("onMove(%.2f, %.2f, %s), dx: %.2f, dy: %.2f", x, y, palmFacing, dx, dy));
+
+        switch (palmFacing) {
+            case DOWNWARDS: /* grabbing */
+                pdfView.moveRelativeTo(dx * (-1) * PAN_FACTOR, dy * PAN_FACTOR);
+                refreshWhenIdle();
+                break;
+            case UPWARDS: /* zooming */
+                float zoom = dy * ZOOM_FACTOR;
+
+                if ((zoom > ZOOM_THRESHOLD && pdfView.getZoom() > MIN_ZOOM) || (zoom < -ZOOM_THRESHOLD && pdfView.getZoom() < MAX_ZOOM)) {
+                    pdfView.zoomCenteredRelativeTo(1f-zoom, new PointF(pdfView.getWidth() / 2, pdfView.getHeight() / 2));
+                    refreshWhenIdle();
+                }
+                break;
+            case SIDEWAYS: /* neutral (hovering) */
+            case INCONCLUSIVE:
+        }
+
+        airmouseX = x;
+        airmouseY = y;
+        pdfView.invalidate(); // for onDraw
+    }
+
+    @Override
+    public void onClick() {
+        Log.d(TAG, "airmouse 'click'");
+        airmouseValid = false;
+        engine.startAirmouse(); /* this resets the origin to the current orientation */
+    }
+
+    /* only for debug purposes, register this object as onDraw Listener */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+        if (airmouseActive) {
+            float pointerX = canvas.getClipBounds().centerX() + (canvas.getClipBounds().width() * airmouseX / 180.0f) / 2.f;
+            float pointerY = canvas.getClipBounds().centerY() - (canvas.getClipBounds().height() * airmouseY / 90.0f) / 2.f;
+            float radius = canvas.getClipBounds().width() * 0.1f;
+            float band_height_radius = radius * 0.30f;
+            float band_width_radius = radius * 0.7f;
+            float band_radius = band_height_radius * 0.8f;
+
+            canvas.drawCircle(
+                    pointerX,
+                    pointerY,
+                    radius, pointerPaint);
+
+            if (airmouseMode == AirmousePalmDirection.DOWNWARDS) {
+                canvas.drawRoundRect(
+                        pointerX - band_width_radius,
+                        pointerY - band_height_radius - radius,
+                        pointerX + band_width_radius,
+                        pointerY + band_height_radius - radius,
+                        band_radius,
+                        band_radius,
+                        bandPaint);
+            } else if (airmouseMode == AirmousePalmDirection.UPWARDS) {
+                canvas.drawRoundRect(
+                        pointerX - band_width_radius,
+                        pointerY - band_height_radius + radius,
+                        pointerX + band_width_radius,
+                        pointerY + band_height_radius + radius,
+                        band_radius,
+                        band_radius,
+                        bandPaint);
+            } else if (airmouseMode == AirmousePalmDirection.SIDEWAYS) {
+                canvas.drawRoundRect(
+                        pointerX - band_height_radius + radius,
+                        pointerY - band_width_radius,
+                        pointerX + band_height_radius + radius,
+                        pointerY + band_width_radius,
+                        band_radius,
+                        band_radius,
+                        bandPaint);
+            }
+        }
+    }
+}
